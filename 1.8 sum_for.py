ed8bc314ab2b6125cf403c5522da7a7935477ec9
Yongkyu Lee <37127633+Pig-Rabbit@users.noreply.github.com>
2021-09-09 00:06:05 +0900

    Delete 1.8 sum_for.py

--- 1.8 sum_for.py
+++
@@ -1,8 +0,0 @@
-# sum of integer from 1 to n (for)
-print('solve the sum of integer from 1 to n')
-n = int(input('enter the value of n: '))
-sum = 0
-for i in range(1, n+1):
-    sum += i # add i to sum
-
-print(f'the sum of integer from 1 to {n} is {sum}')
