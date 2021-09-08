96ba00657a2194f605848fe8d0eaf2e2283f69ca
Yongkyu Lee <37127633+Pig-Rabbit@users.noreply.github.com>
2021-09-09 00:05:36 +0900

    Delete 1.3 judge_sign.py

--- 1.3 judge_sign.py
+++
@@ -1,8 +0,0 @@
-# judge sign of input integer
-n = int(input('enter integer: '))
-if n > 0:
-    print('the integer is positive integer')
-elif n < 0:
-    print('the integer is nagative integer')
-else:
-    print('the integer is zero')
